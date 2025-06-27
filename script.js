027bfef015c5c38622ccfe0b2fbf86fbcb453390
Kunchala Yaswanth Kumar <yaswanthkunchala@gmail.com>
2025-06-27 18:02:43 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll=function(){
-    var navbar=document.querySelector(".navbar");
-    if(window.scrollY>50){
-        navbar.style.backgroundColor="black";
-    }
-    else{
-  navbar.style.backgroundColor="transparent";
-    }
-    };
